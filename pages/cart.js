e0454c16f2cef0267e19773a5f11ed16a2642534
Sebastian Bigatton <sebastianbigatton@gmail.com>
2025-01-13 12:03:01 -0300

    add cart page tests

--- pages/cart.js
+++ pages/cart.js
@@ -9,9 +9,14 @@ export class CartPage extends ProductsPage {
         super(page);
         this.page = page;
         this.title = 'Your Cart';
+        this.quantitySelector = '[data-test="item-quantity"]';
         this.continueShoppingButton = page.getByText('Continue Shopping', { exact: true });
         this.checkOutButton = page.getByText('Checkout', { exact: true });
     }
+
+    getProductQuantity(name){
+        return super.getProductItem(name).locator(this.quantitySelector);
+   }
 
     async checkout() {
         await this.checkOutButton.click();
---
+++ tests/cart-page.spec.js
@@ -0,0 +1,97 @@
+const { test, expect } = require('@playwright/test');
+const { LoginPage } = require('../pages/login');
+const { Header } = require('../pages/header');
+const { ProductsPage } = require('../pages/products');
+const { ProductPage } = require('../pages/product');
+const { CartPage } = require('../pages/cart');
+const { CheckoutStepOnePage } = require('../pages/checkout-step-one');
+const products = require('../data/products.json');
+const account = require('../data/account.json');
+
+const productA = products.find(product => product.name === 'Sauce Labs Bike Light');
+const productB = products.find(product => product.name === 'Sauce Labs Bolt T-Shirt');  
+
+test.describe('Cart page', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/');
+        const loginPage = new LoginPage(page);
+        await loginPage.login(account.username, account.password);
+        const header = new Header(page);
+        const productsPage = new ProductsPage(page);
+        expect(header.pageTitle).toHaveText(productsPage.title);      
+        await productsPage.addProductToCart(productA.name);
+        await productsPage.addProductToCart(productB.name);
+        await expect(header.cartButtonCounter).toHaveText('2');
+    });
+
+    test('should display added products as expected', async ({ page }) => {
+        const header = new Header(page);
+        await header.goToCart();
+        const cartPage = new CartPage(page);
+        expect(header.pageTitle).toHaveText(cartPage.title);
+
+        // Sauce Labs Bike Light
+        expect(cartPage.getProductDescription(productA.name)).toHaveText(productA.description);
+        expect(cartPage.getProductPrice(productA.name)).toHaveText(`$${productA.price}`);
+        expect(cartPage.getProductQuantity(productA.name)).toHaveText('1');
+        expect(cartPage.getProductButton(productA.name)).toHaveText('Remove');
+        
+        // Sauce Labs Bolt T-Shirt
+        expect(cartPage.getProductDescription(productB.name)).toHaveText(productB.description);
+        expect(cartPage.getProductPrice(productB.name)).toHaveText(`$${productB.price}`);
+        expect(cartPage.getProductQuantity(productB.name)).toHaveText('1');
+        expect(cartPage.getProductButton(productB.name)).toHaveText('Remove');
+    });
+
+    test('should remove products', async ({ page }) => {
+        const header = new Header(page);
+        await header.goToCart();
+        const cartPage = new CartPage(page);
+        expect(header.pageTitle).toHaveText(cartPage.title);
+
+        await cartPage.removeProduct(productA);
+        expect(cartPage.getProductItem(productA)).toBeHidden();
+        
+        expect(cartPage.getProductDescription(productB.name)).toHaveText(productB.description);
+        expect(cartPage.getProductPrice(productB.name)).toHaveText(`$${productB.price}`);
+        expect(cartPage.getProductQuantity(productB.name)).toHaveText('1');
+        expect(cartPage.getProductButton(productB.name)).toHaveText('Remove');
+
+        await expect(header.cartButtonCounter).toHaveText('1');
+    });
+
+    test('should continue shopping', async ({ page }) => {
+        const header = new Header(page);
+        await header.goToCart();
+        const cartPage = new CartPage(page);
+        expect(header.pageTitle).toHaveText(cartPage.title);
+
+        await cartPage.continueShopping();
+        const productsPage = new ProductsPage(page);
+        expect(header.pageTitle).toHaveText(productsPage.title);
+    });
+
+    test('should navigate to product', async ({ page }) => {
+        const header = new Header(page);
+        await header.goToCart();
+        const cartPage = new CartPage(page);
+        expect(header.pageTitle).toHaveText(cartPage.title);
+
+        await cartPage.goToProduct(productA);
+        const productPage = new ProductPage(page);
+        expect(header.pageTitle).toHaveText(productPage.title);
+        expect(productPage.productName).toHaveText(productA);
+        expect(productPage.productAddToCarButton).toHaveText('Remove');
+    });
+
+    test('should checkout', async ({ page }) => {
+        const header = new Header(page);
+        await header.goToCart();
+        const cartPage = new CartPage(page);
+        expect(header.pageTitle).toHaveText(cartPage.title);
+        
+        await cartPage.checkout();
+        const checkoutStepOnePage = new CheckoutStepOnePage(page);
+        expect(header.pageTitle).toHaveText(checkoutStepOnePage.title);
+    });
+});
